a36bcf3c13b9832ae09ebded2df40cef4be51122
Shaun Hossain <107396795+shaun4727@users.noreply.github.com>
2025-04-30 23:30:22 +0600

    added antd and removed tailwind shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,7 @@
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
-
 export default function Home() {
 	return (
 		<div>
-			<Card>
-				<CardHeader>
-					<CardTitle>Card Title</CardTitle>
-					<CardDescription>Card Description</CardDescription>
-				</CardHeader>
-				<CardContent>
-					<p>Card Content</p>
-				</CardContent>
-				<CardFooter>
-					<p>Card Footer</p>
-				</CardFooter>
-			</Card>
+			<h1>Home page</h1>
 		</div>
 	);
 }
