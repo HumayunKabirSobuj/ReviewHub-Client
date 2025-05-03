8b5bed2f9c9ab64f7e4b391e1a920eccdbaa609b
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2025-05-03 12:47:23 +0600

    admin dashbaord sidebar added

--- src/app/(withCommonLayout)/layout.tsx
+++ src/app/(withCommonLayout)/layout.tsx
@@ -3,7 +3,7 @@ import { Navbar } from "@/components/shared/Navbar";
 const CommonLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <Navbar />
+        <Navbar />
       <div className="container mx-auto">
         <main className="min-h-screen">{children}</main>
       </div>
