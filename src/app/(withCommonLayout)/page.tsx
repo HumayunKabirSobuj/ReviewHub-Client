8090b6e8e09706ddb93e06b659e5da0254b768a7
ikhtiaj-arif <ikhtiaj.arif@gmail.com>
2025-05-23 16:50:19 +0600

    reviews optimized

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -2,18 +2,24 @@ import HeroSection from "@/components/modules/HomePage/HeroSection";
 import HowItWorksSection from "@/components/modules/HomePage/HowItWorksSection";
 import ReviewsSection from "@/components/modules/HomePage/ReviewsSection";
 import ServicesSection from "@/components/modules/HomePage/ServicesSection";
+import { getAllPublishedReviews } from "@/services/Reviews";
 
 export default async function HomePage() {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_API}/review?page=1&limit=6`)
-  const {data:reviewsData} = await res.json()
+  const queryString = new URLSearchParams({
+    page: '1',
+    limit: '6',
+  }).toString();
+
+  const result = await getAllPublishedReviews(queryString);
+  const reviewsData = result?.data ?? [];
+
 
   return (
     <div className="relative w-full min-h-screen  overflow-hidden ">
       {/* <Banner/> */}
       <HeroSection />
       <section className="container mx-auto bg-white px-4 md:px-8 lg:px-16 py-8 md:py-12">
-      <ReviewsSection reviewsData={reviewsData} />
+        <ReviewsSection reviewsData={reviewsData} />
         <ServicesSection />
         <HowItWorksSection />
       </section>
